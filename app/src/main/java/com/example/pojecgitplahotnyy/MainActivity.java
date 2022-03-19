3003bb86e244a55f4a77f9985d09821573750be9
hvrs <hvrsttk@gmail.com>
2022-03-19 13:36:16 +0700

    ...

--- app/src/main/java/com/example/pojecgitplahotnyy/MainActivity.java
+++ app/src/main/java/com/example/pojecgitplahotnyy/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+//231
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
